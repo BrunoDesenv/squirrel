8f19c9252f5901a4b9e388682e636dc793e58b34
Bruno Santos <brunoj.santos91@gmail.com>
2017-10-29 13:56:06 -0200

    Alterado a estrutura do projeto

--- Infra/Web/Program.cs
+++ Infra/Web/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Squirrel
+namespace Web
 {
     public class Program
     {
--- Infra/Web/Startup.cs
+++ Infra/Web/Startup.cs
@@ -9,7 +9,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Squirrel
+namespace Web
 {
     public class Startup
     {
