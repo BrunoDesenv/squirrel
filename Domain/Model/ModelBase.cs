3aa54e4bbce5421883d982b62f89a42ef48ff776
unknown <aciolecarmo@gmail.com>
2017-10-30 18:34:29 -0200

    tipo bool

--- Domain/Model/ModelBase.cs
+++ Domain/Model/ModelBase.cs
@@ -2,6 +2,5 @@ public class ModelBase
 {
     public Guid Id { get; set; }
     public DateTime DateAdd { get; set; }
-    public int Ative { get; set; }
-    
+    public bool Ative { get; set; }
 }
